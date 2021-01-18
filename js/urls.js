39533d1686ef0ed5390027215a395562bd63f715
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2021-01-18 17:25:01 +0100

    oops

--- js/urls.js
+++ js/urls.js
@@ -1,2 +1,3 @@
 // original: "https://api.hearthstonejson.com/v1/70986/enUS/cards.collectible.json"
 const URL_CARD_COLLECTIBLE = "https://raw.githubusercontent.com/kaisadilla/the-hearthstone-project/main/assets/json/cards.collectible.json";
+const URL_EXPANSIONS = "https://raw.githubusercontent.com/kaisadilla/the-hearthstone-project/main/assets/json/expansions.json";
