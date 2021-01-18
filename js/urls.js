cbc48c102339b3ea9653539054397064344432f9
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2021-01-18 17:16:37 +0100

    expansions

---
+++ js/urls.js
@@ -0,0 +1,2 @@
+// original: "https://api.hearthstonejson.com/v1/70986/enUS/cards.collectible.json"
+const URL_CARD_COLLECTIBLE = "https://raw.githubusercontent.com/kaisadilla/the-hearthstone-project/main/assets/json/cards.collectible.json";
